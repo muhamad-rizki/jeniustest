c1e22e75522e0a5fa140e56fbf4b8a911cce38ed
Muhamad Rizki <muhrizkie88@gmail.com>
2019-07-10 23:08:31 +0700

    fix reset navigation

--- src/env.js
+++ src/env.js
@@ -3,6 +3,6 @@ import { StackActions, NavigationActions } from 'react-navigation';
 export const TEST_CONST = '';
 
 export function ResetNavigator(navigation, routeName, params) {
-  const resetAction = NavigationActions.navigate({ routeName, params, key: null });
-  navigation.dispatch(resetAction);
+  const resetAction = NavigationActions.navigate({ routeName, params });
+  navigation.dispatch(StackActions.reset({ index: 0, actions: [resetAction], key: null }));
 }
