aa5dbaf382398a23e3fb05ee74bcc51dff07bc03
Muhamad Rizki <muhrizkie88@gmail.com>
2019-07-11 14:54:48 +0700

    add delete contact

--- src/modules/contactadd/ContactaddView.js
+++ src/modules/contactadd/ContactaddView.js
@@ -8,12 +8,36 @@ import {
   TextField,
   Button,
 } from 'react-native-ui-lib';
-import { ActivityIndicator } from 'react-native';
-import { ScrollView, TouchableNativeFeedback } from 'react-native-gesture-handler';
+import { ActivityIndicator, StyleSheet, Alert } from 'react-native';
+import { ScrollView, TouchableNativeFeedback, TouchableOpacity } from 'react-native-gesture-handler';
 import { colors } from '../../styles';
 import { Header, Icon } from '../../components';
 
 type Props = {};
+
+const styles = StyleSheet.create({
+  fab: {
+    width: 56,
+    height: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    elevation: 4,
+    backgroundColor: colors.blue,
+  }
+});
+
+const getTitleHeader = (isView, isEdit) => {
+  if (isView) return `${isView.firstName} ${isView.lastName}`;
+  if (isEdit) {
+    let name = `${isEdit.firstName} ${isEdit.lastName}`;
+    if (name.length > 25) {
+      name = `${name.substr(0, 25)}...`;
+    }
+    return `Editing ${name}`;
+  }
+  return 'Add new contact';
+};
 
 export default (props: Props) => {
   const {
@@ -21,17 +45,24 @@ export default (props: Props) => {
     firstName,
     lastName,
     age,
+    photo,
+    id,
     errors,
     setFirstName,
     setLastName,
     setAge,
     submit,
     loading,
+    enableEdit,
+    editMode,
+    deleteContact,
   } = props;
+  const isView = navigation.getParam('view', false) && !editMode ? navigation.getParam('view', false) : false;
+  const isEdit = editMode ? navigation.getParam('view', false) : false;
   return (
     <View flex>
       <Header
-        title="Add New Contact"
+        title={getTitleHeader(isView, isEdit)}
         color={colors.blue}
         textColor={colors.white}
         rightComponent={(
@@ -63,12 +94,14 @@ export default (props: Props) => {
           }}
         >
           <Avatar
+            imageSource={photo ? { uri: photo } : null}
             label={AvatarHelper.getInitials(`${firstName} ${lastName}`)}
             size={80}
           />
         </View>
         <View padding-16>
           <TextField
+            editable={!isView}
             floatingPlaceholder
             placeholder="First Name"
             enableErrors
@@ -78,6 +111,7 @@ export default (props: Props) => {
             onChangeText={setFirstName}
           />
           <TextField
+            editable={!isView}
             floatingPlaceholder
             placeholder="Last Name"
             enableErrors
@@ -87,41 +121,121 @@ export default (props: Props) => {
             onChangeText={setLastName}
           />
           <TextField
+            editable={!isView}
             floatingPlaceholder
             placeholder="Age"
             enableErrors
             keyboardType="numeric"
             maxLength={3}
-            value={age}
+            value={age.toString()}
             error={!errors.age ? '' : errors.age}
             onChangeText={setAge}
           />
         </View>
-        <View bottom center>
-          <View>
-            <Button
-              disabled={loading}
-              label="Submit"
-              backgroundColor={colors.primary}
-              color={loading ? colors.gray : colors.white}
-              onPress={() => submit({ firstName, lastName, age })}
-            >
-              {loading
-                ? <ActivityIndicator color={colors.white} style={{ marginRight: 8 }} />
-                : null}
-            </Button>
-          </View>
-          {
-            errors.submit !== ''
-              ? (
-                <View marginV-16>
-                  <Text red>{errors.submit}</Text>
+        {
+          isView ? null
+            : (
+              <View bottom center padding-16>
+                <View flex row center>
+                  {
+                    !isEdit ? null
+                      : (
+                        <View marginH-8 style={{ flex: 0.3 }}>
+                          <Button
+                            disabled={loading}
+                            label="Delete"
+                            backgroundColor={colors.red}
+                            color={loading ? colors.gray : colors.white}
+                            onPress={() => {
+                              Alert.alert(
+                                'Delete Contact',
+                                `Delete ${firstName} ${lastName}?`,
+                                [
+                                  {
+                                    text: 'Cance',
+                                  },
+                                  {
+                                    text: 'Delete',
+                                    onPress: () => deleteContact({
+                                      firstName,
+                                      lastName,
+                                      age,
+                                      photo,
+                                      id,
+                                    }),
+                                  },
+                                ]
+                              );
+                            }}
+                          >
+                            {loading
+                              ? (
+                                <ActivityIndicator
+                                  color={colors.white}
+                                  style={{ marginRight: 8 }}
+                                />
+                              )
+                              : null}
+                          </Button>
+                        </View>
+                      )
+                  }
+                  <View marginH-8 style={{ flex: 0.3 }}>
+                    <Button
+                      disabled={loading}
+                      label="Submit"
+                      backgroundColor={colors.primary}
+                      color={loading ? colors.gray : colors.white}
+                      onPress={() => submit({
+                        firstName,
+                        lastName,
+                        age,
+                        photo,
+                        id,
+                      })}
+                    >
+                      {loading
+                        ? <ActivityIndicator color={colors.white} style={{ marginRight: 8 }} />
+                        : null}
+                    </Button>
+                  </View>
                 </View>
-              )
-              : null
-          }
-        </View>
+                {
+                  errors.submit !== ''
+                    ? (
+                      <View marginV-16>
+                        <Text red center>{errors.submit}</Text>
+                      </View>
+                    )
+                    : null
+                }
+              </View>
+            )
+        }
       </ScrollView>
+      {
+        editMode ? null
+          : (
+            <View
+              style={[
+                {
+                  position: 'absolute',
+                  bottom: 20,
+                  right: 20,
+                },
+                styles.fab
+              ]}
+            >
+              <TouchableOpacity
+                style={styles.fab}
+                onPress={enableEdit}
+                disabled={loading}
+              >
+                <Icon name="pencil" type="MaterialCommunityIcons" size={28} color={colors.white} />
+              </TouchableOpacity>
+            </View>
+          )
+      }
     </View>
   );
 };
